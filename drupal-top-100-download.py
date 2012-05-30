6ca762e86fce413dc5c5f13bfc0d66fecbbe507a
Dan Tracy <djt5019@psu.edu>
2012-05-29 21:03:26 -0300

    Made some style changes to your file.  Made use of some nice features of BeautifulSoup and context managers for opening and auto-closing of the makefile (which will only be opened once during execution).

--- drupal-top-100-download.py
+++ drupal-top-100-download.py
@@ -1,73 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Include the /bs4 directory so we can import it in case the user doesn't have 
-# BeautifulSoup installed already.
 import sys
 sys.path.append("/bs4")
 
-
-# We'll need the Regular Expression library to parse our text
-import re
-
-# The subprocess library lets us run shell commands:
+import urllib2
 from subprocess import call
 
-# Beautiful Soup is an HTML parser: http://www.crummy.com/software/BeautifulSoup/bs4/doc/
+try:
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
 
-# This is for an older version of the libray.
-# from BeautifulSoup import BeautifulSoup
+# Change to another0 value to make it the 'Top n Drupal Modules'
+module_limit = 100
+filename = 'drupal-{limit}-makefile.make'.format(limit=module_limit)
 
-# This is for Version 4 of BeautifulSoup: pip install beautifulsoup4
-from bs4 import BeautifulSoup
+# Header of the makefile
+makefile_preamble = """
+; This makeFile was generated and downloads the top 100 modules
+; from http://drupal.org/project/usage
 
+core = 6.x
+api = 2
 
-# The urllib2 is used to retrieve data from a web page:
-import urllib2
-
-# Tell BeautifulSoup the URL to parse:
-soup = BeautifulSoup(urllib2.urlopen('http://drupal.org/project/usage'))
-
-# Create our text file and add the necessary canned text to it.
-makeFile = open('drupal-100-makefile.make', 'w')
-makeFile.write('; This makeFile was generated and downloads the top 100 modules \n')
-makeFile.write('; from http://drupal.org/project/usage \n')
-makeFile.write('\n')
-makeFile.write('core = 6.x \n')
-makeFile.write('api = 2 \n')
-makeFile.write('\n')
-makeFile.close()
-
-printlist = []
+"""
 
 # Give the user some feedback so they know the program is doing something.
 print "Retrieving module list from drupal.org..."
 
-# Find all the <td> from our URL, then find all the <a> that have an <href>
-# Parse out the first 100 items that match our criteria.
-# This is very specific to the URL provided earlier
-for td in soup.findAll("td"):
-    for a in td.findAll("a", {"href": True}):
-        printlist.append(a['href'])
-for href in printlist:
-	moduleList = printlist[1:101]
-	
+soup = BeautifulSoup(urllib2.urlopen('http://drupal.org/project/usage'))
 
-# Now that we have only the first 100 items(it says 101 because drupal core is first), 
-# we open our previous text file and add each item after we parse out anything in between the 
-# slashes with  a regular expression. We then need to turn this list into a string so we can 
-# write each item to the file on a new line.
-for items in moduleList:
-	makeFile = open('drupal-100-makefile.make', 'a')
-	modules = re.sub("/\w+/\w+/", "", items).strip('[],\'')
-	makeFile.writelines("projects[] = " + (str(modules) + '\n'))
-makeFile.close()
-print "Make File, ", makeFile, " created successfully.\n"
+count = 0
+module_list = []
+for td in soup.findAll("td"):
+    # If our current count exceeds our limit then we have exactly the right
+    # number of modules
+    if count > module_limit:
+        break
+
+    # If the BeautifuSoup tag has an anchor child element then append it to a
+    # list for later use. We don't need to use a find here for the anchors
+    # since BeautifulSoup uses a little black magic to make child nodes
+    # object attributes
+    if td.a:
+        # Split the string from at each occurence of the '/' character
+        # then use the last item in the list it returns
+        mod_name = td.a['href'].split('/')[-1]
+        mod_name = "projects[] = {module}".format(module=mod_name)
+        module_list.append(mod_name)
+        count += 1
+
+# Ignore the first entry since it's always drupal
+module_list = module_list[1:]
+# Open the filename for writing using a context manager.  This will auto close
+# the filename when it leaves scope or an exception is thrown.  It's
+# equivalent to a try, except, finally block.  This also opens the file up
+# once rather than several times which is usually slow.
+with open(filename, 'w') as f:
+    f.writelines(makefile_preamble)
+    f.writelines('\n'.join(module_list))
+
+print "Make File, {name},  created successfully\n".format(name=filename)
 
 # Now that we have a makefile created, we'll use Drush to download all of the modules
 # for us.
 print "Building site...\n"
-call(["drush", "make", "--no-core", "drupal-100-makefile.make", "drupal-modules", "--force-complete", "-y"])
+call(["drush", "make", "--no-core", filename, "drupal-modules", "--force-complete", "-y"])
 print "Drush make complete.\n"
-
-
-
-
